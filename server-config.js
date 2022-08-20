987a57e02d4dc3f1aef6eef5e45c5b1d0aa373f9
ruophin <963941483@qq.com>
2022-08-20 15:52:34 +0800

    init

---
+++ server-config.js
@@ -0,0 +1,15 @@
+// 前端请求URL灵活配置文件
+window.serverConfig = {
+    // 后台服务器
+    VUE_APP_HOST_URL: 'https://astms.u-breath.com/elk-asthma-admin',
+    VUE_APP_MGMT_URL: 'https://astms.u-breath.com/elk-asthma-mgmt',
+    // VUE_APP_HOST_URL: 'https://tstscreening.u-breath.cn/elk-asthma-admin',
+    // VUE_APP_MGMT_URL: 'https://tstscreening.u-breath.cn/elk-asthma-mgmt',
+    // VUE_APP_MGMT_URL: 'http://192.168.1.4:8089/elk-asthma-mgmt',//华仲
+    // VUE_APP_HOST_URL: 'http://192.168.1.4:8080/elk-asthma-admin',//华仲
+    // VUE_APP_HOST_URL: 'http://192.168.1.54:8088/elk-asthma-admin',
+    // VUE_APP_HOST_URL: 'http://192.168.1.12:8080/elk-asthma-admin',
+    // VUE_APP_HOST_URL: 'http://192.168.1.35:8080/elk-asthma-admin'
+    // VUE_APP_HOST_URL: 'http://192.168.1.16:8080/elk-asthma-admin'
+    // VUE_APP_HOST_URL: 'http://116.62.174.209:8081/elk-asthma-admin',
+}
